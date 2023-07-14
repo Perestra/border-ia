0cac84da293f82dfc5ab1fe9cdbd039cb2724069
Perestra <danilo.ps.1996@gmail.com>
2023-07-14 20:35:01 -0300

    Added a new className prop

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import styles from './ItemList.module.scss'
 
-const ItemList = ({ href, text }) => {
+const ItemList = ({ className, href, text }) => {
   return (
     <li className={ styles.list }>
         <a 
-            className={ styles.list__anchor }
+            className={ className }
             href={ href }>{ text }
         </a>
     </li>
