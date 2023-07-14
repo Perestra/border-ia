975269cb578c74b724a8992366ebca8cae5990d7
Perestra <danilo.ps.1996@gmail.com>
2023-07-14 20:35:20 -0300

    Added the Header component in App file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import styles from './App.module.scss'
+import Header from 'components/Header/Header';
 
 function App() {
+
+  const nav = ['Recursos', 'Sobre', 'Depoimentos']
+
   return (
     <div className={ styles.app }>
-      
+      <Header 
+        nav= { nav }
+      />
     </div>
   );
 }
