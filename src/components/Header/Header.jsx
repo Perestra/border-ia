6b6b148d0ae1164038dcb09219a3edfba05c5505
Perestra <danilo.ps.1996@gmail.com>
2023-07-14 20:34:46 -0300

    Created the Header main component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import styles from './Header.module.scss'
+
+import { ReactComponent as Logo } from 'assets/svg/logo.svg'
+import Navigation from 'components/Navigation/Navigation';
+import ItemList from 'components/ItemList/ItemList';
+import Button from 'components/Button/Button';
+
+const Header = ({ nav }) => {
+  return (
+    <header className={ styles.header }>
+        <div className={ styles.header__logo } >
+          <Logo />
+          <h1>Border-IA</h1>
+        </div>
+        <Navigation 
+          list={ nav.map( (item, index) =>  
+            <ItemList
+              key={ index } 
+              className={ styles.header__list }
+              href=''
+              text={ item }
+            />
+          )}
+        />
+        <Button 
+          className={ styles.header__button }
+          type= 'button'
+          value='Comunidade'
+        />
+      </header>
+  )
+}
+
+export default Header
