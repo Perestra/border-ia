faebdd227332fa9c0c3034dd6732a144f09a4b5a
Perestra <danilo.ps.1996@gmail.com>
2023-07-15 21:15:14 -0300

    Removed the Navigation component in the Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import styles from './Header.module.scss'
 
 import { ReactComponent as Logo } from 'assets/svg/logo.svg'
-import Navigation from 'components/Navigation/Navigation';
 import AnchorList from 'components/AnchorList/AnchorList';
 import Button from 'components/Button/Button';
+import { HiOutlineMenuAlt3 } from 'react-icons/hi'
 
 const Header = () => {
 
@@ -12,13 +12,13 @@ const Header = () => {
 
   return (
     <header className={ styles.header }>
-        <div className={ styles.header__logo } >
-          <Logo />
-          <h1>Border-IA</h1>
-        </div>
-        <Navigation 
-          flex='flexStart'
-          list={ nav.map( (item, index) =>  
+      <div className={ styles.header__logo } >
+        <Logo />
+        <h1>Border-IA</h1>
+      </div>
+      <nav className={ styles.header__nav }>
+        <ul className={ styles.header__ul }>
+          { nav.map( (item, index) =>  
             <AnchorList
               key={ index } 
               className={ styles.header__list }
@@ -26,12 +26,16 @@ const Header = () => {
               text={ item }
             />
           )}
-        />
+        </ul>
+      </nav>
+      <div className={ styles.header__mobile }>
         <Button 
           className={ styles.header__button }
           type= 'button'
           value='Comunidade'
         />
+        <HiOutlineMenuAlt3 className={ styles.header__navIcon } />  
+      </div>
     </header>
   )
 }
