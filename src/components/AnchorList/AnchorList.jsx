7dd349ef6acd37b6c285140213f5c47786a95bf0
Perestra <danilo.ps.1996@gmail.com>
2023-07-17 21:47:54 -0300

    Transform target tag into a prop

--- src/components/AnchorList/AnchorList.jsx
+++ src/components/AnchorList/AnchorList.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import styles from './AnchorList.module.scss'
 
-const AnchorList = ({ className, href, text }) => {
+const AnchorList = ({ className, href, target, text }) => {
   return (
     <li className={ styles.list }>
       <a 
         className={ className }
         href={ href }
-        target="_blank"
+        target={ target }
         rel='noreferrer'
       >
         { text }
